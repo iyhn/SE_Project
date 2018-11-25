db3417dca508dd6c462b25c804f5747e6ef6ee19
codexsize <code_wichayut_utrasak@hotmail.com>
2018-11-25 16:26:27 +0700

    Update review & task history box, Update Suggestion box

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -4,6 +4,7 @@ import nuch from '../nuch.jpg';
 import Cross from '../cross.png';
 import moment from 'moment'
 import api from '../api/api';
+import './css/SuggestBox.css';
 
 const cancel = (back) => {
     
@@ -45,76 +46,137 @@ const edit = (setMainState,back) => {
 const Profile = ({closeModal, setMainState, ...props}) => {
     let picpath = '../../' + 'code.jpg';
     return (
-            <div class="profile-area" >
-                <div style={{float:'right',zIndex:1000}} >
-                    
-                </div>
-                <div class="profile-text">Profile</div>
-                <div class="info">
-                    <div class="picture-profile">
-                        <div style={{width:'200px', height:'200px', borderRadius:'50%', overflow:'hidden', marginRight:'20px', marginTop:'15px', marginLeft:'-10px'}}>
-                            <img src={props.profile.picture} alt="Avatar" />
+            <div className='profile-page'>
+                <div class="profile-area" >
+                    <div style={{float:'right',zIndex:1000}} >
+                        
+                    </div>
+                    <div class="profile-text">Profile</div>
+                    <div class="info">
+                        <div class="picture-profile">
+                            <div style={{width:'200px', height:'200px', borderRadius:'50%', overflow:'hidden', marginRight:'20px', marginTop:'15px', marginLeft:'-10px'}}>
+                                <img src={props.profile.picture} alt="Avatar" />
+                            </div>
+                        </div>
+
+                        <div class="detail-area">
+                            <div class="line-1">
+                                <div class="row-header">First Name</div>
+                                <div class="dataout">
+                                    <input name='detail' disabled={true} value={props.profile.firstname} onChange={(e)=>{setMainState({firstname: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Last Name</div>
+                                <div class="dataout">
+                                    <input name='detail' disabled={true} value={props.profile.lastname} onChange={(e)=>{setMainState({lastname: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Date of Birth</div>
+                                <div class="dataout">
+                                    <input name='detail' disabled={true} value={moment(props.profile.dob).format('DD/MM/YYYY')} onChange={(e)=>{setMainState({dob: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Nationality</div>
+                                <div class="dataout">
+                                <input name='detail' disabled={true} value={props.profile.nationality} onChange={(e)=>{setMainState({nationality: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Email</div>
+                                <div class="dataout">
+                                    <input name='detail' disabled={true} value={props.profile.email} onChange={(e)=>{setMainState({email: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Address</div>
+                                <div class="dataout">
+                                    <input name='detail' disabled={true} value={props.profile.address} onChange={(e)=>{setMainState({address: e.target.value})}}></input>
+                                </div>
+                            </div>
+                            <div class="sector-line"></div>
+                            <div class="line-1">
+                                <div class="row-header">Experience</div>
+                                <div class="dataout">
+                                </div>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="detail-area">
-                        <div class="line-1">
-                            <div class="row-header">First Name</div>
-                            <div class="dataout">
-                                <input name='detail' disabled={true} value={props.profile.firstname} onChange={(e)=>{setMainState({firstname: e.target.value})}}></input>
+
+
+                    <div class="buttom-area">
+                        <button type="button" class="cancel" onClick={()=>cancel(setMainState)} value="Cancel">Cancel</button>
+                        <button id='edit' type="button" class="edit" onClick={()=>edit(setMainState,closeModal)}>Edit</button>
+                    </div>
+
+                </div>
+
+                <div className='historypart'>
+                    <div className='profilereviewbox'>
+                        <div className='profilereviewheader'>
+                            Review History
+                        </div>
+                        <div className='historybox'>
+                            <div style={{position: 'relative'}}>
+                            <div className='profileboxshape'>
+                                <div className="hiseditHover">
+                                </div>
+                                <div className='profileeachbox'>
+                                    <div className='profiletopic'>Task name
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <div style={{position: 'relative'}}>
+                            <div className='profileboxshape'>
+                                <div className="hiseditHover">
+                                </div>
+                                <div className='profileeachbox'>
+                                    <div className='profiletopic'>Task name
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+
+                            <div style={{position: 'relative'}}>
+                            <div className='profileboxshape'>
+                                <div className="hiseditHover">
+                                </div>
+                                <div className='profileeachbox'>
+                                    <div className='profiletopic'>Task name
+                                    </div>
+                                </div>
+                            </div>
                             </div>
                         </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Last Name</div>
-                            <div class="dataout">
-                                <input name='detail' disabled={true} value={props.profile.lastname} onChange={(e)=>{setMainState({lastname: e.target.value})}}></input>
-                            </div>
+                    </div>
+                    <div className='profiletaskbox'>
+                        <div className='profiletaskheader'>
+                            Task History
                         </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Date of Birth</div>
-                            <div class="dataout">
-                                <input name='detail' disabled={true} value={moment(props.profile.dob).format('DD/MM/YYYY')} onChange={(e)=>{setMainState({dob: e.target.value})}}></input>
+                        <div className='historybox'>
+                            <div style={{position: 'relative'}}>
+                            <div className='profileboxshape'>
+                                <div className="hiseditHover">
+                                </div>
+                                <div className='profileeachbox'>
+                                    <div className='profiletopic'>Task name
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Nationality</div>
-                            <div class="dataout">
-                            <input name='detail' disabled={true} value={props.profile.nationality} onChange={(e)=>{setMainState({nationality: e.target.value})}}></input>
-                            </div>
-                        </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Email</div>
-                            <div class="dataout">
-                                <input name='detail' disabled={true} value={props.profile.email} onChange={(e)=>{setMainState({email: e.target.value})}}></input>
-                            </div>
-                        </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Address</div>
-                            <div class="dataout">
-                                <input name='detail' disabled={true} value={props.profile.address} onChange={(e)=>{setMainState({address: e.target.value})}}></input>
-                            </div>
-                        </div>
-                        <div class="sector-line"></div>
-                        <div class="line-1">
-                            <div class="row-header">Experience</div>
-                            <div class="dataout">
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-
-                <div class="buttom-area">
-                    <button type="button" class="cancel" onClick={()=>cancel(setMainState)} value="Cancel">Cancel</button>
-                    <button id='edit' type="button" class="edit" onClick={()=>edit(setMainState,closeModal)}>Edit</button>
-                </div>
-
             </div>
     )
 }
--- src/Components/SuggestBox.js
+++ src/Components/SuggestBox.js
@@ -5,78 +5,86 @@ import star from '../star.png';
 import Task from './Task';
 
 const SuggestBox = ({openModal,...props}) => (
-    <div className='taskbox'>
-        <div style={{position: 'relative'}}>
-            <div className='boxshape'>
-                <div className="editHover" onClick={()=>openModal(Task)}>
-                </div>
-                <div className='eachbox'>
-                    <div className='tasktopic'>{props[0].topic}
-                        <div className='picbox'>
-                            <img src={props[0].picture} className='picAuthor'/>
-                        </div>
-                    </div>
-                    <div className='taskwage'>{'Wage : '+props[0].wage}</div>
-                </div>
+    <div>
+        <div className='taskboxheader'>
+            <div className='taskheadertext'>
+                SUGGESTION TASK
             </div>
         </div>
-        <div style={{position: 'relative'}}>
-            <div className='boxshape'>
-                <div className="editHover" onClick={()=>openModal(Task)}>
-                </div>
-                <div className='eachbox'>
-                    <div className='tasktopic'>
-                        {props[1].topic}
-                        <div className='picbox'>
-                            <img src={props[1].picture} className='picAuthor'/>
-                        </div>
+        
+        <div className='taskbox'>
+            <div style={{position: 'relative'}}>
+                <div className='boxshape'>
+                    <div className="taskeditHover" onClick={()=>openModal(Task)}>
                     </div>
-                    <div className='taskwage'>{'Wage : '+props[1].wage}</div>
+                    <div className='eachbox'>
+                        <div className='tasktopic'>{props[0].topic}
+                            <div className='picbox'>
+                                <img src={props[0].picture} className='picAuthor'/>
+                            </div>
+                        </div>
+                        <div className='taskwage'>{'Wage : '+props[0].wage}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style={{position: 'relative'}}>
-            <div className='boxshape'>
-                <div className="editHover" onClick={()=>openModal(Task)}>
-                </div>
-                <div className='eachbox'>
-                    <div className='tasktopic'>
-                        {props[2].topic}
-                        <div className='picbox'>
-                            <img src={props[2].picture} className='picAuthor'/>
-                        </div>
+            <div style={{position: 'relative'}}>
+                <div className='boxshape'>
+                    <div className="taskeditHover" onClick={()=>openModal(Task)}>
                     </div>
-                    <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    <div className='eachbox'>
+                        <div className='tasktopic'>
+                            {props[1].topic}
+                            <div className='picbox'>
+                                <img src={props[1].picture} className='picAuthor'/>
+                            </div>
+                        </div>
+                        <div className='taskwage'>{'Wage : '+props[1].wage}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style={{position: 'relative'}}>
-            <div className='boxshape'>
-                <div className="editHover" onClick={()=>openModal(Task)}>
-                </div>
-                <div className='eachbox'>
-                    <div className='tasktopic'>
-                        {props[2].topic}
-                        <div className='picbox'>
-                            <img src={props[2].picture} className='picAuthor'/>
-                        </div>
+            <div style={{position: 'relative'}}>
+                <div className='boxshape'>
+                    <div className="taskeditHover" onClick={()=>openModal(Task)}>
                     </div>
-                    <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    <div className='eachbox'>
+                        <div className='tasktopic'>
+                            {props[2].topic}
+                            <div className='picbox'>
+                                <img src={props[2].picture} className='picAuthor'/>
+                            </div>
+                        </div>
+                        <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style={{position: 'relative'}}>
-            <div className='boxshape'>
-                <div className="editHover" onClick={()=>openModal(Task)}>
-                </div>
-                <div className='eachbox'>
-                    <div className='tasktopic'>
-                        {props[2].topic}
-                        <div className='picbox'>
-                            <img src={props[2].picture} className='picAuthor'/>
-                        </div>
+            <div style={{position: 'relative'}}>
+                <div className='boxshape'>
+                    <div className="taskeditHover" onClick={()=>openModal(Task)}>
                     </div>
-                    <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    <div className='eachbox'>
+                        <div className='tasktopic'>
+                            {props[2].topic}
+                            <div className='picbox'>
+                                <img src={props[2].picture} className='picAuthor'/>
+                            </div>
+                        </div>
+                        <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    </div>
+                </div>
+            </div>
+            <div style={{position: 'relative'}}>
+                <div className='boxshape'>
+                    <div className="taskeditHover" onClick={()=>openModal(Task)}>
+                    </div>
+                    <div className='eachbox'>
+                        <div className='tasktopic'>
+                            {props[2].topic}
+                            <div className='picbox'>
+                                <img src={props[2].picture} className='picAuthor'/>
+                            </div>
+                        </div>
+                        <div className='taskwage'>{'Wage : '+props[2].wage}</div>
+                    </div>
                 </div>
             </div>
         </div>
